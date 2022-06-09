fc8e2f32cffed7b925ec18b16db631b662d4031d
chilam <chilam.Lee@outlook.com>
2022-06-09 21:35:19 +0800

    :art: style: 优化代码结构

--- src/ContentScript/index.ts
+++ src/ContentScript/index.ts
@@ -2,25 +2,6 @@ import styles from "../ContentScript/index.module.scss";
 import createContents from "../ContentScript/contents";
 
 /**
- * When the user scrolls down 20px from the top of the document, show the button
- */
-window.onscroll = () => {
-  const showBackTopBtn = () => {
-    if (
-      document.body.scrollTop > 200 ||
-      document.documentElement.scrollTop > 200
-    ) {
-      backTopBtn.style.opacity = "1";
-      backTopBtn.style.visibility = "visible";
-    } else {
-      backTopBtn.style.opacity = "0";
-      backTopBtn.style.visibility = "hidden";
-    }
-  };
-  showBackTopBtn();
-};
-
-/**
  * When the user clicks on the button, scroll to the top of the document
  */
 const backToTop = () => {
@@ -32,8 +13,28 @@ const backTopBtn: HTMLButtonElement = document.createElement("button");
 // btn style
 backTopBtn.id = "backTopBtn";
 backTopBtn.textContent = "🔝";
-backTopBtn.addEventListener("click", () => backToTop());
+backTopBtn.addEventListener("click", backToTop);
 backTopBtn.className += `${styles.backTopBtn}`;
+
+const showBackTopBtn = () => {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backTopBtn.style.opacity = "1";
+    backTopBtn.style.visibility = "visible";
+  } else {
+    backTopBtn.style.opacity = "0";
+    backTopBtn.style.visibility = "hidden";
+  }
+};
+
+/**
+ * When the user scrolls down 20px from the top of the document, show the button
+ */
+window.onscroll = () => {
+  showBackTopBtn();
+};
 
 const createBackTopBtn = () => {
   const tag = document.querySelector("body");
